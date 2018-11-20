31796320948bcf52f399d53dfa4ef79de9d9c32d
majormalfunk <32271489+majormalfunk@users.noreply.github.com>
2018-11-20 22:35:02 +0200

    Cucumber/Selenium tests for adding new tips

--- src/test/java/ohtutips/StepDefinitions.java
+++ src/test/java/ohtutips/StepDefinitions.java
@@ -4,6 +4,7 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import java.util.List;
+import ohtutips.domain.BookTip;
 import static org.junit.Assert.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -21,14 +22,29 @@ public class StepDefinitions {
 
     @Given("user has opened the application and link {string} has been clicked")
     public void user_has_opened_the_application_and_link_has_been_clicked(String linkText) {
+        driver.get(baseUrl);
+        WebElement element = driver.findElement(By.linkText(linkText));
+        element.click();
     }
 
-    @When("all necessary book tip field have been filled")
-    public void all_necessary_book_tip_field_have_been_filled() {
+    @When("all necessary book tip fields have been filled")
+    public void all_necessary_book_tip_fields_have_been_filled() {
+        BookTip bookTip = oneBookTest();
+        WebElement element = driver.findElement(By.name("author"));
+        element.sendKeys(bookTip.getAuthor());
+        element = driver.findElement(By.name("title"));
+        element.sendKeys(bookTip.getTitle());
+        element = driver.findElement(By.name("type"));
+        element.sendKeys(bookTip.getType());
+        element = driver.findElement(By.name("isbn"));
+        element.sendKeys(bookTip.getIsbn());
+        element = driver.findElement(By.name("tags"));
+        element.sendKeys(bookTip.getTags());
     }
 
     @When("book tip has been submitted")
     public void book_tip_has_been_submitted() {
+        driver.findElement(By.tagName("form")).submit();
     }
 
     @Then("list of book tips has {int} entries")
@@ -40,14 +56,34 @@ public class StepDefinitions {
 
     @Then("one of them is the newly created one")
     public void one_of_them_is_the_newly_created_one() {
+        BookTip bookTip = oneBookTest();
+        boolean contains = false;
+        WebElement element = driver.findElement(By.id("book-tips"));
+        List<WebElement> list = element.findElements(By.xpath(".//li"));
+        for (WebElement tip : list) {
+            if (tip.getText().contains(bookTip.getIsbn())) {
+                contains = true;
+                break;
+            }
+        }
+        assertTrue(contains);
     }
 
-    @When("all necessary book tip field have not been filled")
-    public void all_necessary_book_tip_field_have_not_been_filled() {
+    @When("all necessary book tip fields have not been filled")
+    public void all_necessary_book_tip_fields_have_not_been_filled() {
+        BookTip bookTip = oneBookTest();
+        WebElement element = driver.findElement(By.name("author"));
+        element.sendKeys(bookTip.getAuthor());
+        element = driver.findElement(By.name("title"));
+        element.sendKeys(bookTip.getTitle());
+        element = driver.findElement(By.name("type"));
+        element.sendKeys(bookTip.getType());
+        element.sendKeys(bookTip.getTags());
     }
 
     @Then("error message {string} is shown")
     public void error_message_is_shown(String errorMsg) {
+        pageHasContent(errorMsg);
     }
 
     @Given("user has opened the application")
@@ -63,6 +99,18 @@ public class StepDefinitions {
 
     private void pageHasContent(String content) {
         assertTrue(driver.getPageSource().contains(content));
+    }
+    
+    private BookTip oneBookTest() {
+        BookTip bookTip = new BookTip();
+        bookTip.setTitle("The Martian");
+        bookTip.setAuthor("Andy Weir");
+        bookTip.setType("Book");
+        bookTip.setIsbn("978-0091956134");
+        bookTip.setTags("Science Fiction");
+        bookTip.setPrerequisiteCourses("");
+        bookTip.setRelatedCourses("");
+        return bookTip;
     }
 
 }
--- src/test/resources/ohtutips/02_add_new.feature
+++ src/test/resources/ohtutips/02_add_new.feature
@@ -2,13 +2,13 @@ Feature: as a user, i can add a book tip to the system.
 
     Scenario: add a new book tip is successful when all necessary information is provided 
         Given user has opened the application and link 'make a new book tip' has been clicked
-        When all necessary book tip field have been filled
+        When all necessary book tip fields have been filled
         And book tip has been submitted
         Then list of book tips has 4 entries
         And one of them is the newly created one
 
     Scenario: adding a new book tip is unsuccessful when all necessary information has not been provided
         Given user has opened the application and link 'make a new book tip' has been clicked
-        When all necessary book tip field have not been filled
+        When all necessary book tip fields have not been filled
         And book tip has been submitted
-        Then error message "Please fill all field marked with (*)." is shown
+        Then error message "Please fill all fields marked with (*)." is shown
