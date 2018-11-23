e853e9920e2f3bcdb19438db6f4938a3c819e786
Aki Utoslahti <aki.utoslahti@gmail.com>
2018-11-23 10:34:18 +0200

    database initialized only in test mode not in production

--- src/main/java/ohtutips/controller/DefaultController.java
+++ src/main/java/ohtutips/controller/DefaultController.java
@@ -1,5 +1,6 @@
 package ohtutips.controller;
 
+import com.zaxxer.hikari.HikariDataSource;
 import javax.annotation.PostConstruct;
 import ohtutips.domain.BookTip;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,43 +17,48 @@ public class DefaultController {
     @Autowired
     private BookTipRepository bookTipRepository;
 
+    @Autowired
+    private HikariDataSource ds;
+
     @PostConstruct
     public void init() {
-        // purge previous data and insert initial data
-        bookTipRepository.deleteAll();
-        
-        BookTip bookTip1 = new BookTip();
-        bookTip1.setTitle("Lord Of The Rings");
-        bookTip1.setAuthor("J. R. R. Tolkien");
-        bookTip1.setType("Book");
-        bookTip1.setIsbn("978-0544003415");
-        bookTip1.setTags("Fantasy");
-        bookTip1.setPrerequisiteCourses("");
-        bookTip1.setRelatedCourses("");
-        
-        bookTipRepository.save(bookTip1);
-        
-        BookTip bookTip2 = new BookTip();
-        bookTip2.setTitle("Silmarillion");
-        bookTip2.setAuthor("J. R. R. Tolkien");
-        bookTip2.setType("Book");
-        bookTip2.setIsbn("978-0345325815");
-        bookTip2.setTags("Fantasy");
-        bookTip2.setPrerequisiteCourses("");
-        bookTip2.setRelatedCourses("");
-        
-        bookTipRepository.save(bookTip2);
 
-        BookTip bookTip3 = new BookTip();
-        bookTip3.setTitle("The Hobbit or There and Back Again");
-        bookTip3.setAuthor("J. R. R. Tolkien");
-        bookTip3.setType("Book");
-        bookTip3.setIsbn("978-0618002214");
-        bookTip3.setTags("Fantasy");
-        bookTip3.setPrerequisiteCourses("");
-        bookTip3.setRelatedCourses("");
-        
-        bookTipRepository.save(bookTip3);
+        if (ds.getJdbcUrl().contains("jdbc:h2:mem:testdb")) {
+
+            BookTip bookTip1 = new BookTip();
+            bookTip1.setTitle("Lord Of The Rings");
+            bookTip1.setAuthor("J. R. R. Tolkien");
+            bookTip1.setType("Book");
+            bookTip1.setIsbn("978-0544003415");
+            bookTip1.setTags("Fantasy");
+            bookTip1.setPrerequisiteCourses("");
+            bookTip1.setRelatedCourses("");
+
+            bookTipRepository.save(bookTip1);
+
+            BookTip bookTip2 = new BookTip();
+            bookTip2.setTitle("Silmarillion");
+            bookTip2.setAuthor("J. R. R. Tolkien");
+            bookTip2.setType("Book");
+            bookTip2.setIsbn("978-0345325815");
+            bookTip2.setTags("Fantasy");
+            bookTip2.setPrerequisiteCourses("");
+            bookTip2.setRelatedCourses("");
+
+            bookTipRepository.save(bookTip2);
+
+            BookTip bookTip3 = new BookTip();
+            bookTip3.setTitle("The Hobbit or There and Back Again");
+            bookTip3.setAuthor("J. R. R. Tolkien");
+            bookTip3.setType("Book");
+            bookTip3.setIsbn("978-0618002214");
+            bookTip3.setTags("Fantasy");
+            bookTip3.setPrerequisiteCourses("");
+            bookTip3.setRelatedCourses("");
+
+            bookTipRepository.save(bookTip3);
+        }
+
     }
 
     @RequestMapping(value = "/*", method = RequestMethod.GET)
@@ -61,6 +67,5 @@ public class DefaultController {
                 new Sort(Sort.Direction.ASC, "id")));
         return "index";
     }
-    
-    
+
 }
